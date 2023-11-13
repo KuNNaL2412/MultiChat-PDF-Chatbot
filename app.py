12a6375c48032fb5a3313aac47fefc0e651137d9
KuNNaL2412 <94506399+KuNNaL2412@users.noreply.github.com>
2023-11-13 14:09:25 +0530

    req

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import dill
 import pickle
 import streamlit as st
 from PyPDF2 import PdfReader
@@ -10,36 +9,47 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
 
 with st.sidebar:
-        st.title('🗨️ PDF Based Chatbot')
-        st.markdown("## Conversation History: ")
+    st.title('🗨️ PDF Based Chatbot')
+    st.markdown("## Conversation History: ")
 
-        if "chat_sessions" not in st.session_state:
-            st.session_state.chat_sessions = {}
+    if "chat_sessions" not in st.session_state:
+        st.session_state.chat_sessions = {}
 
-        if "active_session" not in st.session_state or st.sidebar.button("New Chat"):
-            chat_id = len(st.session_state.chat_sessions) + 1
-            session_key = f"Chat {chat_id}"
-            st.session_state.chat_sessions[session_key] = []
-            st.session_state.active_session = session_key
+    if "active_session" not in st.session_state or st.sidebar.button("New Chat"):
+        chat_id = len(st.session_state.chat_sessions) + 1
+        session_key = f"Chat {chat_id}"
+        st.session_state.chat_sessions[session_key] = []
+        st.session_state.active_session = session_key
 
-        for session in st.session_state.chat_sessions:
-            if st.sidebar.button(session, key=session):
-                st.session_state.active_session = session
-        st.markdown('''
-        ## About App:
+    for session in st.session_state.chat_sessions:
+        if st.sidebar.button(session, key=session):
+            st.session_state.active_session = session
+    st.markdown('''
+    ## About App:
 
-        The app's primary resource is utilised to create:
+    The app's primary resource is utilized to create:
 
-        - [Streamlit](https://streamlit.io/)
-        - [Langchain](https://docs.langchain.com/docs/)
-        - [OpenAI](https://openai.com/)
+    - [Streamlit](https://streamlit.io/)
+    - [Langchain](https://docs.langchain.com/docs/)
+    - [OpenAI](https://openai.com/)
 
-        ## About me:
+    ## About me:
 
-        - [Linkedin](https://www.linkedin.com/in/kunal-pamu-710674230/)
-        
-        ''')
-        st.write("Made by Kunal Shripati Pamu")
+    - [Linkedin](https://www.linkedin.com/in/kunal-pamu-710674230/)
+
+    ''')
+    st.write("Made by Kunal Shripati Pamu")
+
+def save_vector_store(vector_store, file_path):
+    # Save
+    with open(file_path, "wb") as f:
+        pickle.dump(vector_store, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_vector_store(file_path):
+    # Load
+    with open(file_path, "rb") as f:
+        vector_store = pickle.load(f)
+    return vector_store
 
 def main():
     api_key = st.text_input("Enter your OpenAI API Key", type="password")
@@ -66,13 +76,11 @@ def main():
             store_name = pdf.name[:-4]
 
             if os.path.exists(f"{store_name}.pkl"):
-                with open(f"{store_name}.pkl", "rb") as f:
-                    vector_store = dill.load(f)
+                vector_store = load_vector_store(f"{store_name}.pkl")
             else:
                 embeddings=OpenAIEmbeddings()
-                vector_store=FAISS.from_texts(chunks,embedding=embeddings)
-                with open(f"{store_name}.pkl", "wb") as f:
-                    dill.dump(vector_store, f)
+                vector_store=FAISS.from_texts(chunks, embedding=embeddings)
+                save_vector_store(vector_store, f"{store_name}.pkl")
                 
             llm = OpenAI(temperature=0)
             qa_chain = ConversationalRetrievalChain.from_llm(llm, vector_store.as_retriever())
@@ -95,7 +103,6 @@ def main():
                 with st.chat_message("assistant"):
                     st.markdown(response)
                 st.session_state.chat_sessions[st.session_state.active_session].append({"role": "assistant", "content": response})
-
 
 if __name__ == '__main__':
     main()
